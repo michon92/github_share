1bca4a8cf9e32970eaa30c8ee31fa3272c0162b7
michon92 <michalkoduje@gmail.com>
2023-05-19 15:20:16 +0200

    Homework no 11.2 from 'Python Crash Course'

--- city_function.py
+++ city_function.py
@@ -1,5 +1,9 @@
-def get_formatted_city(city, country):
+def get_formatted_city(city, country, population=''):
     """Funkcja generuje nazwę miasta i państwa, w którym się znajduje"""
-    full_city_country = f"{city}, {country}"
+    if population:
+        # Zadanie 11.2
+        full_city_country = f"{city}, {country }, populacja: {str(population)}"
+    else:
+        # Zadanie 11.1
+        full_city_country = f"{city}, {country}"
     return full_city_country.title()
-
--- test_cities.py
+++ test_cities.py
@@ -1,5 +1,4 @@
 # Zadanie 11.1*
-
 import unittest
 from city_function import get_formatted_city
 
@@ -11,6 +10,11 @@ class CityTestCase(unittest.TestCase):
         """Czy wprowadzane dane w postaci 'Kraków, Polska' będą prawidłowe?"""
         formatted_city = get_formatted_city('kraków', 'polska')
         self.assertEqual(formatted_city, 'Kraków, Polska')
+
+    def test_city_country_population(self):
+        """Czy date 'santiago', 'chile' i 'population=5000000' będą OK?"""
+        formatted_city = get_formatted_city('santiego', 'chile', '5000000')
+        self.assertEqual(formatted_city, 'Santiego, Chile, Populacja: 5000000')
 
 
 if __name__ == '__main__':
