56acd6be483e2ed64749f8f03eabbf0d188f10de
Michal <128338418+michon92@users.noreply.github.com>
2023-05-19 15:14:11 +0200

    Delete city_population_function.py

--- city_population_function.py
+++
@@ -1,4 +0,0 @@
-def get_formatted_city(city, country, population):
-    """Funkcja generuje nazwę miasta i państwa, w którym się znajduje"""
-    full_city_country = f"{city}, {country}"
-    return full_city_country.title()
